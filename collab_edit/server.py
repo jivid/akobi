8461f1e015949bf882ebf4f0da791482e28e7320
Sakshi Jain <s28jain@uwaterloo.ca>
2014-05-16 15:51:08 -0400

    Implemented Server

--- collab_edit/server.py
+++ collab_edit/server.py
@@ -1,15 +1,92 @@
-import tornado.ioloop
-import tornado.web
+import json
+
+from tornado.web import RequestHandler, Application
+from tornado.websocket import WebSocketHandler
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+class States:
+    INITIAL = 1
+    DIFF_WAIT_CL1 = 2
+    ACK_WAIT_CL1 = 3
+    DIFF_WAIT_CL2 = 4
+    ACK_WAIT_CL2 = 5
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
+
+def write_message(message, socket):
+    print("Writing message: " + message)
+    socket.write_message(message)
+
+
+def ask_diff(socket):
+    message = {}
+    message["type"] = 1
+    message["data"] = None
+    write_message(json.dumps(message), socket)
+    SocketHandler.state = SocketHandler.state + 1
+
+
+def wait_for_diff_then_send(diff, socket):
+    message = {}
+    message["type"] = 3
+    message["data"] = diff
+    write_message(json.dumps(message), socket)
+    SocketHandler.state = SocketHandler.state + 1
+
+
+def wait_for_ack_then_ask_diff(socket):
+    message = {}
+    message["type"] = 1
+    message["data"] = None
+    write_message(json.dumps(message), socket)
+    SocketHandler.state = SocketHandler.state + 1
+
+
+def wait_for_ack():
+    SocketHandler.state = States.INITIAL
+
+
+class SocketHandler(WebSocketHandler):
+    sockets = []
+    state = 0
+
+    def open(self):
+        if len(SocketHandler.sockets) >= 2:
+            print("Too many Connections")
+            return
+
+        if self not in SocketHandler.sockets:
+            SocketHandler.sockets.append(self)
+
+        if len(SocketHandler.sockets) == 2:
+            state = States.INITIAL
+            ask_diff(SocketHandler.sockets[1])
+
+    def on_message(self, message):
+        print("got message " + message)
+        if SocketHandler.state == States.DIFF_WAIT_CL1:
+            wait_for_diff_then_send(json.loads(message), SocketHandler.sockets[2])
+
+        elif SocketHandler.state == States.ACK_WAIT_CL1:
+            wait_for_ack_then_ask_diff(SocketHandler.sockets[2])
+
+        elif SocketHandler.state == States.DIFF_WAIT_CL2:
+            wait_for_diff_then_send(json.loads(message), SocketHandler.sockets[1])
+
+        elif SocketHandler.state == States.ACK_WAIT_CL2:
+            wait_for_ack()
+
+        elif SocketHandler.state == States.INITIAL:
+            ask_diff(SocketHandler.sockets[1])
+
+    def on_close(self):
+        pass
+
+application = Application([
+    (r"/", SocketHandler),
 ])
 
 if __name__ == "__main__":
     application.listen(8888)
-    tornado.ioloop.IOLoop.instance().start()
+    IOLoop.instance().start()
