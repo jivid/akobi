7a0705c72ada57704c33b685b7e285ba7da1052f
sdosborn <steven@steven-K52Jr.(none)>
2014-05-22 13:22:51 -0400

    String formatting fixes in heartbeat.py

--- lib/event_handlers/heartbeat.py
+++ lib/event_handlers/heartbeat.py
@@ -14,7 +14,7 @@ class HeartbeatHandler(BaseEventHandler):
         client_id = message['clientID']
         redis = redis_client.get_redis_instance()
         logging.debug(
-            'Got Heartbeat for interview "%s" and client "%s"'
+            "Got Heartbeat for interview '%s' and client '%s'"
             % (interview_id, client_id))
 
         response = {'datetime': str(datetime.datetime.now()),
@@ -26,18 +26,18 @@ class HeartbeatHandler(BaseEventHandler):
         try:
             sockets = interviews[interview_id]
         except KeyError, e:
-            logging.error('Could not find interview ID - reason "%s"' % str(e))
+            logging.error("Could not find interview ID")
 
         found = False
         for socket in sockets:
             if not hasattr(socket, 'client_id'):
-                logging.error('Socket in interview "%s" has no client ID'
+                logging.error("Socket in interview '%s' has no client ID"
                               % (interview_id))
             if str(socket.client_id) == client_id:
                 socket.write_message(json.dumps(response))
                 found = True
         if not found:
-            logging.error('No client with id "%s" in interview with id "%s"'
+            logging.error("No client with id '%s' in interview with id '%s'
                           % (client_id, interview_id))
 
         redis.hset("heartbeat", client_id, message['datetime'])
