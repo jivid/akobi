3ac0c1c3c40b8687bbcb3118bbefb417dd0404d0
jbrick <jbrick91@gmail.com>
2014-05-26 15:30:24 -0400

    Started working on basic front-end prototype.

--- handlers/index.py
+++ handlers/index.py
@@ -1,6 +1,26 @@
 from tornado.web import RequestHandler
 
 
+class InterviewHandler(RequestHandler):
+    def get(self, *args, **kwargs):
+        self.render('../templates/interview.html')
+
+
 class IndexHandler(RequestHandler):
     def get(self, *args, **kwargs):
         self.render('../templates/index.html')
+
+
+class SetupHandler(RequestHandler):
+    def get(self, *args, **kwargs):
+        try:
+            notes = self.get_query_argument('notes')
+        except:
+            notes = "off"
+        try:
+            collab_code = self.get_query_argument('collab_code')
+        except:
+            collab_code = "off"
+        self.render(
+            '../templates/setup_complete.html',
+             notes=notes, collab_code=collab_code)
