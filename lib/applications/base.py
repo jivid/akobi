de1913f3630095d50d5755ac8e7834cfbb02f87a
Divij Rajkumar <drajkuma1@gmail.com>
2014-05-26 16:25:44 -0400

    Add on_join to base application class

--- lib/applications/base.py
+++ lib/applications/base.py
@@ -2,5 +2,14 @@ class BaseApplication(object):
     """
     Base class that all applications will inherit from.
     """
-    def handle_message(cls, *args, **kwargs):
+    def handle_message(self, *args, **kwargs):
         raise NotImplementedError
+
+    def on_join(self, *args, **kwargs):
+        """
+        Override this no-op if you need your application to perform some
+        initialization before you can begin handling client messages.
+        Called every time a new client joins an interview in which your
+        application is registered.
+        """
+        pass
