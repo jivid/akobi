346790992a1a217763ad4f95dec0334306378238
Warren Smith <sharpsw@gmail.com>
2014-05-26 14:48:48 -0400

    Added Sakshi's suggestion.

--- __init__.py
+++ __init__.py
@@ -8,17 +8,21 @@ logger-configuration-to-log-to-file-and-print-to-stdout
 import logging
 
 log_formatter = logging.Formatter("%(asctime)s [%(threadName)s]"
-                                 + " [%(levelname)s] %(message)s")
+                                  + " [%(levelname)s] %(message)s")
+
 root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
 
 file_handler = logging.FileHandler(filename='akobi_output.log')
+file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(log_formatter)
+
 root_logger.addHandler(file_handler)
 
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(log_formatter)
-root_logger.addHandler(console_handler)
+console_handler.setLevel(logging.INFO)
 
-root_logger.setLevel(logging.INFO)
+root_logger.addHandler(console_handler)
 
 log = root_logger
