94f7a6ca3a01f6d83839c9eead8e9d0502bcc26d
jbrick <jbrick91@gmail.com>
2014-05-30 16:15:44 -0400

    Added  dynamic app selection.

--- handlers/index.py
+++ handlers/index.py
@@ -1,30 +1,34 @@
 from tornado.web import RequestHandler
 
 from akobi.lib.utils import make_random_string
+from akobi.lib.applications.registry import registry
+
 
 class InterviewHandler(RequestHandler):
     def get(self, *args, **kwargs):
-        self.render('../templates/interview.html')
+        self.render(
+            '../templates/interview.html', applications=self.request.arguments)
 
 
 class IndexHandler(RequestHandler):
     def get(self, *args, **kwargs):
-        self.render('../templates/index.html')
+        self.render('../templates/index.html', applications=registry.available)
 
 
 class SetupHandler(RequestHandler):
     def get(self, *args, **kwargs):
 
         # HTML checkboxes pass nothing if they are unchecked.
-        notes = self.get_query_argument('notes', default="off")
-        collab_code = self.get_query_argument('collab_code', default="off")
+        application_state = {}
+        for application in registry.available:
+            application_state[application] = self.get_query_argument(
+                application, default="off")
 
         # TODO: We should probably do this more like a product serial than
         # just a random id.
         interview_id = make_random_string(length=30)
 
-        # Currently the notes /  collab_code variables are just displayed back
-        # to the user, and not used for anything.
         self.render(
             '../templates/setup_complete.html',
-             notes=notes, collab_code=collab_code, interview_id=interview_id)
+            interview_id = interview_id,
+            application_state = application_state)
