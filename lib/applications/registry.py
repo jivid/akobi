21c89c2d5b43ee6859bf8790e59d60a0f03e70d1
Divij Rajkumar <drajkuma1@gmail.com>
2014-07-07 10:21:08 -0400

    Revert "Client now "requires" application JS based on list sent from server."

--- lib/applications/registry.py
+++ lib/applications/registry.py
@@ -69,10 +69,6 @@ class ApplicationRegistry(object):
                     self._non_essential_apps.append(self.available[key])
         return self._non_essential_apps
 
-    def app_names_for_interview(self, interview_id):
-        return self.interviews[interview_id].keys()\
-            if interview_id in self.interviews else None
-
     def apps_for_interview(self, interview_id):
         return self.interviews[interview_id]\
             if interview_id in self.interviews else None
