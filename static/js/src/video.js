bfcb16cc5b7f6746b57d55b654faa7ac76340826
Divij Rajkumar <drajkuma1@gmail.com>
2014-07-11 22:29:26 -0400

    Reactify video

--- static/js/src/video.js
+++ static/js/src/video.js
@@ -1,122 +1,159 @@
-define(["ext/videoadapter", "util"], function(videoAdapter, util) {
+/** @jsx React.DOM **/
+define(["common", "ext/videoadapter", "util"], function(common, videoAdapter, util) {
 
-    var SET_CALLER = 1;
-    var SIGNALLING = 2;
+    var setupVideo = function() {
+        var SET_CALLER = 1;
+        var SIGNALLING = 2;
 
-    var pc_config = {
-      'iceServers': [
-        {
-          'url': 'stun:stun.l.google.com:19302'
+        var pc_config = {
+            'iceServers': [
+                {
+                'url': 'stun:stun.l.google.com:19302'
+                }
+            ]
         }
-      ]
-    }
 
-    var localVideo;
-    var remoteVideo;
-    var localStream;
-    var remoteStream;
-    var pc;
+        var localVideo;
+        var remoteVideo;
+        var localStream;
+        var remoteStream;
+        var pc;
 
-    var errorCallback = function(error){
-        util.throwException(error);
-    }
-
-    var waitForPC = function(callback, arg){
-        if(pc == undefined){
-            setTimeout(function(){
-                waitForPC(callback, arg);
-            }, 1000);
-        }else{
-            callback(arg);
+        var errorCallback = function(error){
+            util.throwException(error);
         }
-    }
 
-    var createOffer = function(){
-        pc.createOffer(setLocalAndSend, errorCallback);
-    }
+        var waitForPC = function(callback, arg){
+            if(pc == undefined){
+                setTimeout(function(){
+                    waitForPC(callback, arg);
+                }, 1000);
+            }else{
+                callback(arg);
+            }
+        }
 
-    var respondToOffer = function(offer){
-        pc.setRemoteDescription(new RTCSessionDescription(offer), function(){
-             pc.createAnswer(setLocalAndSend, errorCallback)
-        }, errorCallback);
-    }
+        var createOffer = function(){
+            pc.createOffer(setLocalAndSend, errorCallback);
+        }
 
-    var addIceCandidate = function(candidate){
-        pc.addIceCandidate(candidate);
-    }
+        var respondToOffer = function(offer){
+            pc.setRemoteDescription(new RTCSessionDescription(offer), function(){
+                pc.createAnswer(setLocalAndSend, errorCallback)
+            }, errorCallback);
+        }
 
-    var setLocalAndSend = function(sessionDescription){
-        pc.setLocalDescription(sessionDescription);
-        interview.socket.send({
-            type: 'video',
-            clientID: interview.client.id,
-            interviewID: interview.id,
-            data: {
-                type: SIGNALLING,
-                data: { sessionDescription : sessionDescription}
+        var addIceCandidate = function(candidate){
+            pc.addIceCandidate(candidate);
+        }
+
+        var setLocalAndSend = function(sessionDescription){
+            pc.setLocalDescription(sessionDescription);
+            interview.socket.send({
+                type: 'video',
+                clientID: interview.client.id,
+                interviewID: interview.id,
+                data: {
+                    type: SIGNALLING,
+                    data: { sessionDescription : sessionDescription}
+                }
+            });
+        }
+
+        EventBus.on("video", function(msg) {
+            console.log("Video message");
+            switch (msg.data.type){
+                case SET_CALLER:
+                    console.log("Setting caller");
+                    if(msg.data.data.isCaller){
+                        waitForPC(createOffer);
+                    }
+                    break;
+                case SIGNALLING:
+                    if (msg.data.data.sessionDescription){
+                        if (msg.data.data.sessionDescription.type == "offer"){
+                            console.log("responding to offer");
+                            waitForPC(respondToOffer, msg.data.data.sessionDescription);
+                        } else if (msg.data.data.sessionDescription.type == "answer"){
+                            console.log("answer");
+                            pc.setRemoteDescription(new RTCSessionDescription
+                            (msg.data.data.sessionDescription));
+                        }
+                    }
+                    else if (msg.data.data.type == "candidate"){
+                        console.log("New candidate");
+                        var candidate = new RTCIceCandidate({
+                            sdpMLineIndex: msg.data.data.label,
+                            candidate: msg.data.data.candidate,
+                            sdpMid: msg.data.data.id
+                        });
+                        waitForPC(addIceCandidate, candidate);
+                    }
+                    break;
             }
         });
-    }
 
-    EventBus.on("video", function(msg) {
-        switch (msg.data.type){
-            case SET_CALLER:
-                if(msg.data.data.isCaller){
-                    waitForPC(createOffer);
-                }
-                break;
-            case SIGNALLING:
-                if (msg.data.data.sessionDescription){
-                    if (msg.data.data.sessionDescription.type == "offer"){
-                        waitForPC(respondToOffer, msg.data.data.sessionDescription);
-                    } else if (msg.data.data.sessionDescription.type == "answer"){
-                        pc.setRemoteDescription(new RTCSessionDescription
-                        (msg.data.data.sessionDescription));
+        window.videoAdapter = videoAdapter;
+        videoAdapter.getUserMedia(
+            {video: true, audio : true}, $.proxy(function(localMediaStream) {
+                console.log("Setting local stream");
+                console.log(localMediaStream);
+                localStream = localMediaStream;
+                localVideo = $('#local_video');
+                localVideo.attr("src", window.URL.createObjectURL(localMediaStream));
+                pc = new videoAdapter.RTCPeerConnection(pc_config);
+
+                console.log("Adding local stream");
+                pc.addStream(localStream);
+
+                remoteVideo = $('#remote_video');
+                pc.onaddstream = function(event){
+                    remoteVideo.attr("src", window.URL.createObjectURL(event.stream));
+                    remoteStream = event.stream
+                };
+
+                pc.onicecandidate = function(event){
+                    if (event.candidate){
+                        interview.socket.send({
+                            type: 'video',
+                            clientID: interview.client.id,
+                            interviewID: interview.id,
+                            data: {
+                                type: SIGNALLING,
+                                data: { type : "candidate", label : event.candidate
+                                .sdpMLineIndex, id: event.candidate.sdpMid, candidate
+                                : event.candidate.candidate}
+                            }
+                        });
                     }
                 }
-                else if (msg.data.data.type == "candidate"){
-                    var candidate = new RTCIceCandidate({sdpMLineIndex : msg
-                    .data.data.label, candidate : msg.data.data.candidate,
-                    sdpMid : msg.data.data.id});
-                    waitForPC(addIceCandidate, candidate);
-                }
-                break;
+            }, this), errorCallback);
+    };
+
+    var VideoSpace = React.createClass({
+        render: function() {
+            return (
+                <div>
+                    <video id="local_video" width="200" height="200" autoPlay="true" muted="true"></video>
+                    <video id="remote_video" width="400" height="400" autoPlay="true"></video>
+                </div>
+            )
         }
     });
 
-    videoAdapter.getUserMedia(
-        {video: true, audio : true}, $.proxy(function(localMediaStream){
-           localStream = localMediaStream;
-           localVideo= $('#local_video');
-           remoteVideo= $('#remote_video');
-           localVideo.attr("src", window.URL
-                .createObjectURL(localMediaStream));
-           pc = new videoAdapter.RTCPeerConnection(pc_config);
+    var VideoView = common.AkobiApplicationView.extend({
+        initialize: function() {
+            this.render();
+            setupVideo();
+        },
 
-           pc.addStream(localStream);
+        render: function() {
+            React.renderComponent(<VideoSpace />, this.$el.get(0));
+            $('#app-space').append(this.$el);
+        }
+    });
 
-           pc.onaddstream = function(event){
-                remoteVideo.attr("src", window.URL.createObjectURL(event
-                .stream));
-                remoteStream = event.stream
-           };
-
-           pc.onicecandidate = function(event){
-            if (event.candidate){
-                interview.socket.send({
-                    type: 'video',
-                    clientID: interview.client.id,
-                    interviewID: interview.id,
-                    data: {
-                        type: SIGNALLING,
-                        data: { type : "candidate", label : event.candidate
-                        .sdpMLineIndex, id: event.candidate.sdpMid, candidate
-                         : event.candidate.candidate}
-                    }
-                });
-            }
-           }
-        }, this), errorCallback);
-
+    window.setupVideo = setupVideo;
+    new VideoView();
 });
 
