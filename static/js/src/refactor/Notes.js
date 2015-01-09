52e42303dd4e40e71c4f75704ae4b62889f1c354
Warren Smith <sharpsw@gmail.com>
2015-01-09 14:51:37 -0500

    Reconfigured notes to look more like notes.

--- static/js/src/refactor/Notes.js
+++ static/js/src/refactor/Notes.js
@@ -1,13 +1,28 @@
 /** @jsx React.DOM */
 
+var AceEditor = require('./components/AceEditor');
+var Container = require('./components/Container');
 var React = require('react');
 
 var Notes = React.createClass({
 
   render: function() {
+
+    var containerStyle = {
+      'border': '1px solid black',
+    }
+
     return (
       <div>
-        <p>Rendered</p>
+        <Container centered={true} style={containerStyle}>
+          <AceEditor
+            name="notebox"
+            editorWidth={800}
+            editorHeight={150}
+            showLineNumbers={false}
+            showEditorControls={false}
+          />
+        </Container>
       </div>
     );
   }
